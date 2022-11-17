2d9a3f0812a6646790eccf53d7c72c6c83a77813
Nico <portelanicolas2001@gmail.com>
2022-11-17 19:48:18 +0100

    Lector implementado y probado con QR

--- Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/MainActivity.kt
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.liceolapaz.dam.pruebalector
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.client.android.Intents
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+import com.liceolapaz.dam.pruebalector.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        //Abrir el escaner
+        val inicarEscaner = registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
+            //Recuperar los datos obtenidos del escaner
+            if (result.contents == null) {
+                val originalIntent = result.originalIntent
+                if (originalIntent == null) {
+                    //Mensaje al cerrar el escaner manualmente
+                    Toast.makeText(this@MainActivity, "Cancelado", Toast.LENGTH_LONG).show()
+                } else if (originalIntent.hasExtra(Intents.Scan.MISSING_CAMERA_PERMISSION)) {
+                    //Error si el usuario no concede los permisos de la camara
+                    Toast.makeText(this@MainActivity, "No hay permiso para acceder a la cámara", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                //Toast.makeText(this@MainActivity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                viewBinding.fldEscaneo.text = result.contents
+            }
+        }
+
+
+        viewBinding.btnEscaner.setOnClickListener() {
+            val opciones = ScanOptions()
+            opciones.setPrompt("Coloca el codigo dentro del recuadro")
+            inicarEscaner.launch(opciones)
+        }
     }
 }
