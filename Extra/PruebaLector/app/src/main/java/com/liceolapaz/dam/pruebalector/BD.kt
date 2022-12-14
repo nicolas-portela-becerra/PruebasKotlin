d4882f97502c499e6d73f1e117aad5fc0b76bde3
Nico <portelanicolas2001@gmail.com>
2022-12-14 19:29:35 +0100

    mas

--- Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/BD.kt
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/BD.kt
@@ -1,141 +1,71 @@
 package com.liceolapaz.dam.pruebalector
 
-import android.os.StrictMode
+
 import android.util.Log
 import com.zaxxer.hikari.HikariDataSource
 import java.sql.DriverManager
 import java.sql.SQLException
+import java.sql.SQLNonTransientConnectionException
 import java.util.concurrent.Callable
 
-class BD : Callable<ArrayList<String>>{
 
-    companion object {
+class BD : Callable<ArrayList<String>> {
         fun getConexionMYSQL(): ArrayList<String> {
-            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-            StrictMode.setThreadPolicy(policy)
             val array = arrayListOf<String>()
             //DAO buscar
             //Metodo para el main y dentro de ese metodo la conexion a la base de datos
             try {
-                Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
-                Log.i(":::", "Driver bien")
+                Class.forName("com.mysql.cj.jdbc.Driver")
             } catch (e: ClassNotFoundException) {
-                Log.i(":::", "Error en el driver")
                 e.printStackTrace()
             }
             try {
                 //var conexion = DriverManager.getConnection("jdbc:mysql://10.0.2.2/dam22", "nico", "1234")
-                var conexion = DriverManager.getConnection("jdbc:mysql://192.168.19.47/dam22", "nico", "1234")
+                val conexion = DriverManager.getConnection("jdbc:mysql://192.168.1.176/dam22", "nico", "1234")
                 if(!(conexion == null)) {
-                    Log.i(":::", "Conexion buena")
                     val ps = conexion.prepareStatement("SELECT * FROM alumno")
-                    Log.i(":::", "Ejecutar preparedStatement")
                     val resultados = ps.executeQuery()
-                    Log.i(":::", "Resultados")
 
 
                     while(resultados.next()) {
-                        Log.i(":::", "Resultados: " + resultados.getInt(1))
-                        array.add(resultados.getInt(1).toString())
-                        Log.i(":::", "Resultados: " + resultados.getString(2))
+                        //array.add(resultados.getInt(1).toString())
                         array.add(resultados.getString(2))
-                        Log.i(":::", "Resultados: " + resultados.getString(3))
                         array.add(resultados.getString(3))
                     }
                     return array
                 }
             }
-            catch (e: SQLException) {
-                Log.i(":::", "F total")
+            catch (e: SQLNonTransientConnectionException) {
                 e.printStackTrace()
+            }
+            catch (e1 : SQLException) {
+                e1.printStackTrace()
             }
             return array
         }
-        fun getConexionPOSTGRE(): ArrayList<String> {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
-                Log.i(":::", "Driver bien")
-            } catch (e: ClassNotFoundException) {
-                Log.i(":::", "Error en el driver")
-                e.printStackTrace()
-            }
-            val array = arrayListOf<String>()
-            // create a dataSource
-            val dataSource = HikariDataSource()
-
-            // set the jdbcUrl
-            dataSource.jdbcUrl = "jdbc:mysql://192.168.19.47/dam22"
-
-            // set the username
-            dataSource.username = "nico"
-
-            // set the password
-            dataSource.password = "1234"
-
-
-            // get a connection
-            val connection = dataSource.connection
-
-            // the query is only prepared not executed
-            val query = connection.prepareStatement("SELECT * FROM alumno")
-
-            // the query is executed and results are fetched
-            val resultados = query.executeQuery()
-
-
-            while (resultados.next()) {
-                Log.i(":::", "Resultados: " + resultados.getInt(1))
-                array.add(resultados.getInt(1).toString())
-                Log.i(":::", "Resultados: " + resultados.getString(2))
-                array.add(resultados.getString(2))
-                Log.i(":::", "Resultados: " + resultados.getString(3))
-                array.add(resultados.getString(3))
-            }
-
-            return array
-        }
-
-    }
 
     override fun call(): ArrayList<String> {
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
-        val array = arrayListOf<String>()
-        //DAO buscar
-        //Metodo para el main y dentro de ese metodo la conexion a la base de datos
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
-            Log.i(":::", "Driver bien")
-        } catch (e: ClassNotFoundException) {
-            Log.i(":::", "Error en el driver")
-            e.printStackTrace()
-        }
-        try {
-            //var conexion = DriverManager.getConnection("jdbc:mysql://10.0.2.2/dam22", "nico", "1234")
-            var conexion = DriverManager.getConnection("jdbc:mysql://192.168.19.47/dam22", "nico", "1234")
-            if(!(conexion == null)) {
-                Log.i(":::", "Conexion buena")
-                val ps = conexion.prepareStatement("SELECT * FROM alumno")
-                Log.i(":::", "Ejecutar preparedStatement")
-                val resultados = ps.executeQuery()
-                Log.i(":::", "Resultados")
+        val data = HikariDataSource()
+        data.jdbcUrl = "jdbc:mysql://192.168.1.176/dam22"
+        data.username = "nico"
+        data.password = "1234"
+        val conexion = data.connection
 
+        Log.i(":::", "Conexion hecha")
+        // the query is only prepared not executed
+        val query = conexion.prepareStatement("SELECT * FROM alumno")
 
-                while(resultados.next()) {
-                    Log.i(":::", "Resultados: " + resultados.getInt(1))
-                    array.add(resultados.getInt(1).toString())
-                    Log.i(":::", "Resultados: " + resultados.getString(2))
-                    array.add(resultados.getString(2))
-                    Log.i(":::", "Resultados: " + resultados.getString(3))
-                    array.add(resultados.getString(3))
-                }
-                return array
-            }
+        // the query is executed and results are fetched
+        val result = query.executeQuery()
+
+        // an empty list for holding the results
+        var lista = ArrayList<String>()
+        while (result.next()) {
+            var s = result.getString(3)
+            lista.add(s)
+            println(s)
         }
-        catch (e: SQLException) {
-            Log.i(":::", "F total")
-            e.printStackTrace()
-        }
-        return array
+
+        return lista
     }
 }
--- Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/MainActivity.kt
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.liceolapaz.dam.pruebalector
 
 
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
-import android.net.ConnectivityManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.zxing.client.android.Intents
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
 import com.liceolapaz.dam.pruebalector.databinding.ActivityMainBinding
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import java.util.concurrent.FutureTask
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,20 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            // Si no tiene el permiso, solicita al usuario que lo otorgue
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET), 1)
-        }
-
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (connectivityManager.isDefaultNetworkActive) {
-            // La aplicación tiene acceso a Internet
-
-            viewBinding.txtInternet.setText("Hay internet")
-        } else {
-            // La aplicación no tiene acceso a Internet
-            viewBinding.txtInternet.setText("No hay internet")
-        }
 
         //Abrir el escaner
         val inicarEscaner = registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
@@ -60,20 +38,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        //val conexion = BD.getConexionMYSQL()
-        //val conexion = BD.getConexionPOSTGRE()
-        var pool = Executors.newSingleThreadExecutor()
-        var hilo = BD()
-        val conexion = pool.submit(hilo).get()
-        if(!(conexion.isEmpty())){
-            var string = ""
-            for(s in conexion) {
-                string += s
+
+        viewBinding.boton.setOnClickListener {
+            val bd = BD()
+            val futuro = FutureTask(bd)
+            var hiloBd = Thread(futuro)
+            hiloBd.start()
+            val datos = futuro.get()
+            if(datos.isNotEmpty()){
+                var string = ""
+                for(s in datos) {
+                    string += s
+                }
+                viewBinding.resultados.text = string
             }
-            viewBinding.resultados.text = string
-        }
-        else {
-            viewBinding.resultados.setText("Resulatados vacio")
+            else {
+                viewBinding.resultados.setText("Resultados vacio")
+            }
         }
 
         viewBinding.btnEscaner.setOnClickListener {
