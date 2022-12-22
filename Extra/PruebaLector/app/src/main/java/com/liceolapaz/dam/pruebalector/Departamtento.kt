826373c1139b4a4ed17fdc286eb8db23dde7a674
unknown <portelanicolas2001@gmail.com>
2022-12-22 13:24:31 +0100

    Data clases

---
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/Departamtento.kt
@@ -0,0 +1,7 @@
+package com.liceolapaz.dam.pruebalector
+
+data class Departamtento(
+    var cod : Int,
+    var nom : String,
+    var pais : String
+)
---
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/Empleado.kt
@@ -0,0 +1,10 @@
+package com.liceolapaz.dam.pruebalector
+
+data class Empleado(
+    var cod : Int,
+    var nom : String,
+    var apellido : String,
+    var puesto : String,
+    var salario : Int,
+    var codDepartamento : Int
+)
