2e22dcfbfb6205c5bacea7d2164edf48e0e72370
Nico <portelanicolas2001@gmail.com>
2022-12-23 13:12:56 +0100

    Actualizadas clases para insertar datos en la BD

--- Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/Departamentos.kt
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/Departamentos.kt
@@ -1,4 +1,19 @@
 package com.liceolapaz.dam.pruebalector
 
-class Departamentos {
+import java.sql.Connection
+import java.util.concurrent.Callable
+import java.util.concurrent.FutureTask
+
+class Departamentos(depart : Departamtento) : Callable<Int> {
+    val conexion = FutureTask(BD()).get()
+    val depart = depart
+    override fun call(): Int {
+        val ps = conexion.prepareStatement("Insert INTO Departamento VALUES (?, ?, ?)")
+
+        ps.setInt(1, depart.cod)
+        ps.setString(2, depart.nom)
+        ps.setString(3, depart.pais)
+
+        return ps.executeUpdate()
+    }
 }
--- Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/Empleados.kt
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/Empleados.kt
@@ -1,11 +1,23 @@
 package com.liceolapaz.dam.pruebalector
 
+import java.sql.Connection
 import java.util.concurrent.Callable
+import java.util.concurrent.FutureTask
 
-class Empleados : Callable<ArrayList<Empleado>> {
+class Empleados(empleado : Empleado) : Callable<Int> {
+    val conexion = FutureTask(BD()).get()
+    val empleado = empleado
 
-    override fun call(): ArrayList<Empleado> {
-        TODO("Not yet implemented")
+    override fun call(): Int {
+        val ps = conexion.prepareStatement("INSERT INTO Empleado VALUES (?, ?, ?, ?, ?, ?)")
+
+        ps.setInt(1, empleado.cod)
+        ps.setString(2, empleado.nom)
+        ps.setString(3, empleado.apellido)
+        ps.setString(4, empleado.puesto)
+        ps.setInt(5, empleado.salario)
+        ps.setInt(5, empleado.codDepartamento)
+        return 1
     }
 
 }
