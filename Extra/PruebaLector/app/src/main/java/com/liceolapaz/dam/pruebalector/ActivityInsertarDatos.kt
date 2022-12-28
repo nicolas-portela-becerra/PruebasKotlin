f59bd2419fcdf72786c4a8160828117ba546b54a
Nico <portelanicolas2001@gmail.com>
2022-12-28 18:37:34 +0100

    Cambios para el funcionamiento del Nav menu

--- Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/ActivityInsertarDatos.kt
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/ActivityInsertarDatos.kt
@@ -1,6 +1,5 @@
 package com.liceolapaz.dam.pruebalector
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -9,28 +8,31 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationView
 import com.google.zxing.client.android.Intents
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
-import com.liceolapaz.dam.pruebalector.databinding.ActivityInsertarDatosBinding
-import androidx.fragment.app.add
+import com.liceolapaz.dam.pruebalector.databinding.ActivityInsertarDatosNavBinding
+
 
 class ActivityInsertarDatos : AppCompatActivity() {
-    private lateinit var viewBinding : ActivityInsertarDatosBinding
+    private lateinit var binding : ActivityInsertarDatosNavBinding
     private var x1 : Float = 0F
     private var y1 : Float = 0F
     private var x2: Float = 0F
     private var y2: Float = 0F
-    //
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityInsertarDatosBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
-
+        binding = ActivityInsertarDatosNavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.include.toolbar)
+        showNav()
 
         //Abrir el escaner
         val inicarEscaner = registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
@@ -46,31 +48,51 @@ class ActivityInsertarDatos : AppCompatActivity() {
                 }
             } else {
                 //Toast.makeText(this@MainActivity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-                viewBinding.fldEscaneo.text = result.contents
+                binding.include.fldEscaneo.text = result.contents
             }
         }
 
-        viewBinding.spTablas.onItemSelectedListener = object : OnItemSelectedListener {
+        binding.include.spTablas.onItemSelectedListener = object : OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 supportFragmentManager.commit {
                     setReorderingAllowed(true)
-                    if(viewBinding.spTablas.selectedItem.toString().equals("departamento", true)) {
+                    if(binding.include.spTablas.selectedItem.toString().equals("departamento", true)) {
                         add<FragmentDepartamento>(R.id.fragmentContainer)
                     }
-                    if(viewBinding.spTablas.selectedItem.toString().equals("empleado", true)) {
+                    if(binding.include.spTablas.selectedItem.toString().equals("empleado", true)) {
                         add<FragmentEmpleado>(R.id.fragmentContainer)
                     }
                 }
             }
         }
 
-        viewBinding.btnEscaner.setOnClickListener {
+        binding.include.btnEscaner.setOnClickListener {
             val opciones = ScanOptions()
             opciones.setPrompt("Coloca el codigo dentro del recuadro")
             inicarEscaner.launch(opciones)
         }
+    }
+
+    private fun showNav() {
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.include.toolbar, R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { item ->
+            Log.i(":::", "Listener")
+            Log.i(":::", "Item: " + item)
+            when (item.toString()) {
+                "Recuperar datos"-> {
+                    Log.i(":::", "recuperar")
+                    startActivity(Intent(this@ActivityInsertarDatos, ActivityRecuperarDatos::class.java))
+                    this.finish()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
     }
 
     override fun onTouchEvent(touchEvent: MotionEvent): Boolean {
@@ -82,10 +104,8 @@ class ActivityInsertarDatos : AppCompatActivity() {
             MotionEvent.ACTION_UP -> {
                 x2 = touchEvent.x
                 y2 = touchEvent.y
-                Log.i(":::", "Valor $x1 y Valor $x2")
-                if (x1 > x2 + 200) {
-                    startActivity(Intent(this@ActivityInsertarDatos, ActivityRecuperarDatos::class.java))
-                    this.finish()
+                if ((x1 <= 40F) and (x2 <= 200F)) {
+                    showNav()
                 }
             }
         }
--- Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/ActivityRecuperarDatos.kt
+++ Extra/PruebaLector/app/src/main/java/com/liceolapaz/dam/pruebalector/ActivityRecuperarDatos.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.navigation.NavigationView
 import com.liceolapaz.dam.pruebalector.databinding.ActivityRecuperarDatosBinding
+import com.liceolapaz.dam.pruebalector.databinding.ActivityRecuperarDatosNavBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ActivityRecuperarDatos : AppCompatActivity() {
-    private lateinit var binding : ActivityRecuperarDatosBinding
+    private lateinit var binding : ActivityRecuperarDatosNavBinding
     private var x1 : Float = 0F
     private var y1 : Float = 0F
     private var x2: Float = 0F
@@ -24,34 +28,54 @@ class ActivityRecuperarDatos : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRecuperarDatosBinding.inflate(layoutInflater)
+        binding = ActivityRecuperarDatosNavBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.include.toolbar)
+        showNav()
 
-        binding.btnResultados.setOnClickListener {
+        binding.include.btnResultados.setOnClickListener {
             lifecycleScope.launch {
-                if(binding.fldTablas.text.isEmpty()){
+                if(binding.include.fldTablas.text.isEmpty()){
                     Toast.makeText(this@ActivityRecuperarDatos, "Introduce un nombre para buscar", Toast.LENGTH_SHORT).show()
                 }
                 else {
-                    binding.progressbar.visibility = View.VISIBLE
+                    binding.include.progressbar.visibility = View.VISIBLE
                     lifecycleScope.launch(Dispatchers.IO) {
-                        datos = LeerBd(binding.fldTablas.text.toString(), this@ActivityRecuperarDatos).call()
-                        if(!datos.isEmpty()) {
-                            if(binding.fldTablas.text.toString().equals("departamento", true)) {
+                        datos = LeerBd(binding.include.fldTablas.text.toString(), this@ActivityRecuperarDatos).call()
+                        if(datos.isNotEmpty()) {
+                            if(binding.include.fldTablas.text.toString().equals("departamento", true)) {
                                 mostrarDepartamentos(datos)
                             }
-                            if(binding.fldTablas.text.toString().equals("empleado", true)) {
+                            if(binding.include.fldTablas.text.toString().equals("empleado", true)) {
                                 mostrarEmpleados(datos)
                             }
                         }
                         withContext(Dispatchers.Main) {
-                            binding.progressbar.visibility = View.GONE
+                            binding.include.progressbar.visibility = View.GONE
                         }
                     }
                 }
             }
         }
     }
+
+    private fun showNav() {
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.include.toolbar, R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.toString()) {
+                "Insertar datos"-> {
+                    startActivity(Intent(this@ActivityRecuperarDatos, ActivityInsertarDatos::class.java))
+                    this.finish()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
+    }
+
     override fun onTouchEvent(touchEvent: MotionEvent): Boolean {
         when (touchEvent.action) {
             MotionEvent.ACTION_DOWN -> {
@@ -61,9 +85,8 @@ class ActivityRecuperarDatos : AppCompatActivity() {
             MotionEvent.ACTION_UP -> {
                 x2 = touchEvent.x
                 y2 = touchEvent.y
-                if (x1 + 200 < x2) {
-                    startActivity(Intent(this@ActivityRecuperarDatos, ActivityInsertarDatos::class.java))
-                    this.finish()
+                if ((x1 <= 40F) and (x2 <= 200F)) {
+                    showNav()
                 }
             }
         }
