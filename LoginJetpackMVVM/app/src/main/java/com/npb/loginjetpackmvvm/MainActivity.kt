43325ff22e867cd8a1eb39e90dbb648ed236308c
Nico <portelanicolas2001@gmail.com>
2023-02-14 18:57:46 +0100

    Esqueleto proyecto Jetpack

---
+++ LoginJetpackMVVM/app/src/main/java/com/npb/loginjetpackmvvm/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.npb.loginjetpackmvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.npb.loginjetpackmvvm.ui.theme.LoginJetpackMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginJetpackMVVMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginJetpackMVVMTheme {
+        Greeting("Android")
+    }
+}
